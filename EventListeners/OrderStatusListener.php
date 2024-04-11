4bc141a66d565f92c26e8e56823ba119ea371a14
mdevaud <mdevaud@openstudio.fr>
2024-04-11 14:32:43 +0200

    fix: OrderStatusListener.php propage event for parent Order

--- EventListeners/OrderStatusListener.php
+++ EventListeners/OrderStatusListener.php
@@ -3,15 +3,18 @@
 namespace SubOrderGenerator\EventListeners;
 
 use SubOrderGenerator\Service\SubOrderService;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Core\Event\Order\OrderEvent;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Model\OrderStatus;
+use Thelia\Model\OrderStatusQuery;
 
 class OrderStatusListener implements EventSubscriberInterface
 {
     public function __construct(
-        private SubOrderService $subOrderService
+        private SubOrderService $subOrderService,
+        private EventDispatcherInterface $eventDispatcher
     )
     {
     }
@@ -19,9 +22,11 @@ class OrderStatusListener implements EventSubscriberInterface
     public function postOrderUpdate(OrderEvent $event){
         $order = $event->getOrder();
         if($this->subOrderService->isSubOrder($order->getId()) && $order->isPaid()){
-            $this->subOrderService->updateParentOrderStatus($order->getId(), OrderStatus::CODE_PAID);
+            $parentOrder = $this->subOrderService->updateParentOrderStatus($order->getId(), OrderStatus::CODE_PAID);
+            $event = new OrderEvent($parentOrder);
+            $event->setStatus(OrderStatusQuery::getPaidStatus()->getId());
+            $this->eventDispatcher->dispatch($event, TheliaEvents::ORDER_UPDATE_STATUS);
         }
-
     }
 
     public static function getSubscribedEvents()
